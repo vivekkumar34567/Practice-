6795ab08a4614df5622efed1347d35876fe3b070
vivekkumar34567 <vk20225379@gmail.com>
2024-10-04 06:31:06 +0000

    first commit

---
+++ my-app/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="m-5" style={{color:"blue", display:"flex", justifyContent:"center", alignItem:"center"}}>Hello guys</h1>
+    </>
+  );
+}
